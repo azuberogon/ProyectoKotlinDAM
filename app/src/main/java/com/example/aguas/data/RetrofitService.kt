b298184e42d9f15fcc9fe3910111131f0bafa30e
ZuberoClase <azuberogondam1b@gmail.com>
2024-04-29 20:04:01 +0200

    ultimo

--- app/src/main/java/com/example/aguas/data/RetrofitService.kt
+++ app/src/main/java/com/example/aguas/data/RetrofitService.kt
@@ -1,5 +1,12 @@
 package com.example.aguas.data
 
-class RetrofitService {
-//falta una url por meter en la cual es la base de https://developer.themoviedb.org/3 o algo asi tienes que mirarlo 
+import retrofit2.http.GET
+
+interface RetrofitService {
+//falta una url por meter en la cual es la base de https://developer.themoviedb.org/3 o algo asi tienes que mirarlo
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun listPopularMovies(
+
+    )
+
 }
