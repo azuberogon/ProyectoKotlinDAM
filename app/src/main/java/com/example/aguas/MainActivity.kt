d384062ad4cbd31ab13350ef56ad70d4dddbf3cf
azuberogon <azuberogondam1b@gmail.com>
2024-05-07 19:12:45 +0200

    cambios en el menu
    -falta el menú de tres puntos
    -modificación de algunos menús

--- app/src/main/java/com/example/aguas/MainActivity.kt
+++ app/src/main/java/com/example/aguas/MainActivity.kt
@@ -7,13 +7,22 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnSiguiente: Button
+    private lateinit var btnRegistrar:  Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnSiguiente = findViewById(R.id.btnSiguiente)
 
+
         btnSiguiente.setOnClickListener{
             val intent = Intent(this@MainActivity, menu::class.java )
+            startActivity(intent)
+        }
+
+        btnRegistrar = findViewById(R.id.btnRegistrar)
+
+        btnRegistrar.setOnClickListener{
+            val intent = Intent(this@MainActivity, Registro::class.java )
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/aguas/Registro.kt
+++ app/src/main/java/com/example/aguas/Registro.kt
@@ -1,11 +1,35 @@
 package com.example.aguas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Registro : AppCompatActivity() {
+    private lateinit var btnSiguiente: Button
+    private lateinit var btnCancelar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+
+
+
+        btnSiguiente = findViewById(R.id.btnSiguiente)
+
+
+        btnSiguiente.setOnClickListener{
+            val intent = Intent(this@Registro, menu::class.java )
+            startActivity(intent)
+            finish()
+        }
+
+        btnCancelar = findViewById(R.id.btnCancelar)
+
+        btnCancelar.setOnClickListener{
+            finish()
+            val intent = Intent(this@Registro, MainActivity::class.java )
+            startActivity(intent)
+
+        }
     }
 }
