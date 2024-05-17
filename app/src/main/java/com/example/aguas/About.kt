9ad2820e62fe5fd50893b05dfc0a5db18243637d
azuberogon <azuberogondam1b@gmail.com>
2024-05-17 19:41:34 +0200

    Scroll
    about terminado se ha añadido la funcion para tener un about que puede hacer que el texto se mueva

--- app/src/main/java/com/example/aguas/About.kt
+++ app/src/main/java/com/example/aguas/About.kt
@@ -2,10 +2,27 @@ package com.example.aguas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aguas.adapterText.AdapterText
 
 class About : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+       /* val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val loremIpsum = resources.getString(R.string.sample_contract_text)
+        val loremIpsumArray = loremIpsum.split("\n").toTypedArray()
+
+        val adapter = AdapterText(loremIpsumArray)
+        recyclerView.adapter = adapter
+    */
+        val scrollViewText = findViewById<TextView>(R.id.loremimpsun)
+        val loremIpsum = resources.getString(R.string.sample_contract_text)
+        scrollViewText.text = loremIpsum
+
     }
 }
---
+++ app/src/main/java/com/example/aguas/adapterText/AdapterText.kt
@@ -0,0 +1,28 @@
+package com.example.aguas.adapterText
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aguas.R
+
+class AdapterText(private val dataSet: Array<String>) :
+    RecyclerView.Adapter<AdapterText.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.loremimpsun)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.activity_about, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position]
+    }
+
+    override fun getItemCount() = dataSet.size
+}
