8fca04534a6b1d69956f82f2c225e5f738f2a344
azuberogon <azuberogondam1b@gmail.com>
2024-05-18 19:32:54 +0200

    correccion 2

--- app/src/androidTest/java/com/example/aguas/Prueba_de_GUI.kt
+++ app/src/androidTest/java/com/example/aguas/Prueba_de_GUI.kt
@@ -31,13 +31,56 @@ class MainActivityEspressoTest {
     @Test
     fun testButtonRegistrarNavigation() {
         // Reemplaza el texto en el EditText de nombre de usuario y contraseña
+        onView(withId(R.id.btnSiguiente)).check(matches(isDisplayed())).perform(click())
         onView(withId(R.id.editTxtNombreUsuario)).perform(replaceText("root"))
         onView(withId(R.id.editTxtContraseña)).perform(replaceText("root"))
 
         // Verifica que el botón está en pantalla y haz clic en él
         onView(withId(R.id.btnSiguiente)).check(matches(isDisplayed())).perform(click())
         pressBack()
+        onView(withId(R.id.btnRegistrar)).check(matches(isDisplayed())).perform(click())
+        pressBack()
+
         // Verifica que la actividad de registro se muestra
         onView(withId(R.id.registroLayout)).check(matches(isDisplayed()))
     }
+
+    @Test
+    fun testButtonRegistrarNavigationV2() {
+        // Reemplaza el texto en el EditText de nombre de usuario y contraseña
+        // Verifica que la actividad principal se muestra
+        onView(withId(R.id.mainLayout)).check(matches(isDisplayed()))
+
+        // Verifica que el botón "Siguiente" está en pantalla y haz clic en él
+        onView(withId(R.id.btnSiguiente)).check(matches(isDisplayed())).perform(click())
+
+        // Verifica que se muestra el mensaje de error "Nombre de usuario o contraseña incorrectos"
+        //onView(withText("Nombre de usuario o contraseña incorrectos")).check(matches(isDisplayed()))
+
+        // Reemplaza el texto en el EditText de nombre de usuario y contraseña
+        onView(withId(R.id.editTxtNombreUsuario)).perform(replaceText("root"))
+        onView(withId(R.id.editTxtContraseña)).perform(replaceText("root"))
+
+        // Verifica que el botón "Siguiente" está en pantalla y haz clic en él
+        onView(withId(R.id.btnSiguiente)).check(matches(isDisplayed())).perform(click())
+
+        // Verifica que la actividad de menú se muestra
+        onView(withId(R.id.menuLayout)).check(matches(isDisplayed()))
+
+      /*  // Vuelve a la actividad principal
+        pressBack()
+
+        // Verifica que la actividad principal se muestra
+        onView(withId(R.id.mainLayout)).check(matches(isDisplayed()))
+
+        // Verifica que el botón "Registrar" está en pantalla y haz clic en él
+        onView(withId(R.id.btnRegistrar)).check(matches(isDisplayed())).perform(click())
+
+        // Vuelve a la actividad principal
+        pressBack()
+
+        // Verifica que la actividad de registro se muestra
+        onView(withId(R.id.registroLayout)).check(matches(isDisplayed()))*/
+    }
 }
+
